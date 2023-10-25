d9c8a9c7cc1f9ac18f1bc3d6f93ef5f2dce8b279
t1coz <66951820+t1coz@users.noreply.github.com>
2023-10-25 21:39:28 +0300

    Add files via upload

--- headers/mainwindow.h
+++ headers/mainwindow.h
@@ -2,6 +2,12 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QFileDialog>
+#include <QDir>
+#include <QMessageBox>
+#include <QTableWidgetItem>
+//#include <QtPdf>
+//#include <QtPdf/QPdfPageNavigator>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -16,11 +22,23 @@ public:
     ~MainWindow();
 
 private slots:
-    void on_pushButton_clicked();
-
-
-
     void on_createShelveBtn_clicked();
+
+    void on_deleteShelveBtn_clicked();
+
+    void on_uploadFileBtn_clicked();
+
+    void on_addBookBtn_clicked();
+
+    void on_deleteBookBtn_clicked();
+
+    void on_saveTableBtn_clicked();
+
+    void on_uploadTableBtn_clicked();
+
+    void on_setShelveBtn_clicked();
+
+    void on_openBookBtn_clicked();
 
 private:
     Ui::MainWindow *ui;
